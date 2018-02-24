41fc3fed4f7aed4ad01d067737aa5cb24f558eb8
Robby Dore <rdore88@gmail.com>
2018-02-24 14:34:39 -0500

    Create models for different parts of the response

---
+++ src/main/java/weather/model/Estimated.java
@@ -0,0 +1,8 @@
+
+package weather.model;
+
+
+public class Estimated {
+
+
+}
---
+++ src/main/java/weather/model/Response.java
@@ -0,0 +1,56 @@
+
+package weather.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.lang.Error;
+
+public class Response {
+
+    @SerializedName("version")
+    @Expose
+    private String version;
+    @SerializedName("termsofService")
+    @Expose
+    private String termsofService;
+    @SerializedName("features")
+    @Expose
+    private Features features;
+    @SerializedName("error")
+    @Expose
+    private Error error;
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getTermsofService() {
+        return termsofService;
+    }
+
+    public void setTermsofService(String termsofService) {
+        this.termsofService = termsofService;
+    }
+
+    public Features getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(Features features) {
+        this.features = features;
+    }
+
+    public Error getError() {
+        return error;
+    }
+
+    public void setError(Error error) {
+        this.error = error;
+    }
+
+}
